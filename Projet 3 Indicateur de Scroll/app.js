f0d545ae347b489565f35298753523a5ce10a5af
Ustariz Enzo <enzo.ustariz@hotmail.fr>
2019-08-26 10:52:58 +0200

    Add files via upload

---
+++ Projet 3 Indicateur de Scroll/app.js
@@ -0,0 +1,14 @@
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+    // var winScroll = document.body.scrollTop || document.documentElement.scrollTop; ???
+  var winScroll =  document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+
+
+  console.log(document.documentElement.scrollTop , document.body.scrollTop);
+  
+}
