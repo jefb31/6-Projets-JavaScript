8cf9a4f566acc3e4661f3781feb1798dcdb8170a
Ustariz Enzo <enzo.ustariz@hotmail.fr>
2019-08-27 16:38:06 +0200

    Update app.js

--- Projet 2 Horloge/app.js
+++ Projet 2 Horloge/app.js
@@ -1,32 +1,33 @@
+const secondeDiv = document.querySelector('.seconde')
+const minDiv = document.querySelector('.min')
+const heureDiv = document.querySelector('.heure')
 
-    const secondDiv = document.querySelector('.second-hand');
-    const minDiv = document.querySelector('.min-hand');
-    const hourDiv = document.querySelector('.hour-hand');
+
+function miseEnPlace(){
     
+    const now = new Date();
+//    console.log(now);
     
-    function setDate(){
-        
-        const now = new Date();
-        const seconds = now.getSeconds();
-        const secDeg = ((seconds/60)* 360 + 90);
-        secondDiv.style.transform = `rotate(${secDeg}deg)`;
-        
-        
-        const mins = now.getMinutes();
-        const minDeg = ((mins/60) * 360) + ((seconds/60)*6) + 90;
-        minDiv.style.transform = `rotate(${minDeg}deg)`;
-        
-        
-        const hour = now.getHours();
-        const hourDeg = ((hour/12)*360) + ((mins/60)*30) + 90;
-        hourDiv.style.transform = `rotate(${hourDeg}deg)`;
-        
-        
-    }
+    // l'aiguille des secondes
+    const seconds = now.getSeconds();
+    // console.log(seconds);
+    // la position en degré des secondes actuelles
+    const secDeg = ((seconds/60)*360) + 90;
+    secondeDiv.style.transform = `rotate(${secDeg}deg)`;
     
-  setInterval(setDate,1000);
+    // minutes
+    const mins = now.getMinutes();
+    // 6 deg = 1min
+    const minDeg = ((mins/60)*360) + ((seconds/60)*6) + 90;
+    minDiv.style.transform = `rotate(${minDeg}deg)`;
     
+
+    //heure
+    const hour = now.getHours();
+    // 30 = 1h en deg
+    const hourDeg = ((hour/12)*360) + ((mins/60)*30) + 90;
+    heureDiv.style.transform = `rotate(${hourDeg}deg)`;
+
     
-    
-    
-    
+}
+setInterval(miseEnPlace, 1000);
